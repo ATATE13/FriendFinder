a27edeac11763a574311c7c50072919e18877fd3
atate13 <atate13@yahoo.com>
2019-02-23 19:15:22 -0500

    initial update of the code

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+       "name":"Alyssa Edwards",
+       "photo":"https://media.licdn.com/dms/image/C4E03AQH5vnmeWouNzQ/profile-displayphoto-shrink_800_800/0?e=1556150400&v=beta&t=VzngouKSSfYxdh5PL1SiyWTHq7oEhLZELxu6ZzYuVU4",
+       "scores":[
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+       ]
+    },
+    {
+       "name":"Jamie Delong",
+       "photo":"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/26231278_10215474069224699_978920154395224591_n.jpg?_nc_cat=103&_nc_ht=scontent-mia3-2.xx&oh=a6c4a33af5680240fa7bbb05e39cced8&oe=5CF26F8D",
+       "scores":[
+          "4",
+          "2",
+          "5",
+          "1",
+          "3",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2"
+       ]
+    },
+    {
+       "name":"Amrita Lamba",
+       "photo":"https://media.licdn.com/dms/image/C4D03AQEh4hzDF7MToA/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=I7fVUKMVTVuLCemMZHMW_WpI5NrUp5v79zg2V0qglOU",
+       "scores":[
+          "5",
+          "2",
+          "2",
+          "2",
+          "4",
+          "1",
+          "3",
+          "2",
+          "5",
+          "5"
+       ]
+    },
+    {
+       "name":"Brandon Kalber",
+       "photo":"https://media.licdn.com/dms/image/C4D03AQFEi-BUKhTUDA/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=MXZt1K9KqM5kGeeW68BkV0P4fITrD8aQRamF-8_ugGc",
+       "scores":[
+          "3",
+          "3",
+          "4",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2",
+          "2",
+          "3"
+       ]
+    },
+     {
+        "name":"Emily Sullivan",
+        "photo":"https://media.licdn.com/dms/image/C4E03AQFfHg52_09x9w/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=jbNE76_Q0PDS4iqiQbvRdg3BBClQaokh6KbL-jhT9lY",
+        "scores":[
+           "4",
+           "3",
+           "4",
+           "1",
+           "5",
+           "2",
+           "5",
+           "3",
+           "1",
+           "4"
+        ]
+     },
+     {
+        "name":"Jose Torres",
+        "photo":"https://media.licdn.com/dms/image/C4E03AQFetfzHcScmIA/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=DG0X9BPWesJjo2f9rlkU0hwF_jUenb_3p4BXC_VVyDs",
+        "scores":[
+           "4",
+           "4",
+           "2",
+           "3",
+           "2",
+           "2",
+           "3",
+           "2",
+           "4",
+           "5"
+        ]
+     }
+ 
+ ]
+ 
+ // Here we export the array. This makes it accessible to other files using require. 
+ module.exports = friends;
